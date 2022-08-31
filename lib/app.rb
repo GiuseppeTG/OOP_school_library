d66430c3b573376f0985cd45742cf96e0267b7b6
Giuseppe Tomasini <giusetomasini@gmail.com>
2022-08-31 16:34:51 -0500

    list rentals

--- lib/app.rb
+++ lib/app.rb
@@ -114,7 +114,15 @@ class App
   end
 
   def list_rentals
-    ListRentals.new.list_rentals(@people, @rentals)
+    # ListRentals.new.list_rentals(@people, @rentals)
+    puts 'Enter a person ID'
+    person_id_input = gets.chomp
+    person = @people.find { |p| p['id'] == person_id_input }
+    person_rentals = person['rentals']
+    puts 'This person has no rentals yet' if person_rentals.empty?
+    person_rentals.each do |rental|
+      puts "Name: #{person['name']}, Book: #{rental['book']}, Date: #{rental['date']}"
+    end
     init
   end
 
